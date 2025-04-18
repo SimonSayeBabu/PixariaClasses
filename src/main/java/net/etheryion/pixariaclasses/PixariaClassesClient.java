db2165a78ca58b5e6f4b307d8f4737a878248b87
SimonSayeBabu <simonsathia2004@gmail.com>
2025-04-18 02:45:13 +0200

    first commit

---
+++ src/main/java/net/etheryion/pixariaclasses/PixariaClassesClient.java
@@ -0,0 +1,10 @@
+package net.etheryion.pixariaclasses;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PixariaClassesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/etheryion/pixariaclasses/PixariaClassesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.etheryion.pixariaclasses;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PixariaClassesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
